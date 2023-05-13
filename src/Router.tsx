70024fa220e2f07574580e12724d9499999e8d61
Luca <gabadadzeluca@gmail.com>
2023-05-14 01:16:00 +0400

    add loginPage to the default path route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,10 @@
 import { Route, Routes } from 'react-router-dom'
+import LoginPage from './pages/LoginPage/LoginPage'
 
 export default function Router(){
   return (
   <Routes>
-    <Route path="/" element={<></>} />
+    <Route path="/" element={<LoginPage />} />
     <Route path="/game" element={<></>} />
     <Route path="/leaderboard" element={<></>} />
   </Routes>
