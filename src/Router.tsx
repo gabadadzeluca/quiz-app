84bb25034b03bceda0734dbfbde40e5308389e3d
Luca <gabadadzeluca@gmail.com>
2023-05-14 01:18:53 +0400

    add gamePage to its route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
 import LoginPage from './pages/LoginPage/LoginPage'
+import GamePage from './pages/GamePage/GamePage'
 
 export default function Router(){
   return (
   <Routes>
     <Route path="/" element={<LoginPage />} />
-    <Route path="/game" element={<></>} />
+    <Route path="/game" element={<GamePage />} />
     <Route path="/leaderboard" element={<></>} />
   </Routes>
   )
