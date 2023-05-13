80dc76ea4550251ce662f11c2c86abad01abc04f
Luca <gabadadzeluca@gmail.com>
2023-05-14 00:53:04 +0400

    add context hook for username and its type

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import './App.css'
 import Router from './Router'
+import { createContext } from 'react'
+
+type usernameContextType = {
+  username: string|null;
+}
+
+const Context = createContext<usernameContextType>({username: null});
 
 function App() {
     return (
+      <Context.Provider value={{username: null}}>
       <div>
         <Router />
       </div>
+      </Context.Provider>
+      
     )
 }
 
