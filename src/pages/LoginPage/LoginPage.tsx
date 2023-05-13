a5b7dbba8c01567d98c5e77c17d19e040c54b6e9
Luca <gabadadzeluca@gmail.com>
2023-05-14 01:16:31 +0400

    add handleSubmit function to redirect users to the game

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,20 +1,27 @@
 import { useContext } from 'react'
 import { Context } from '../../App';
+import { useNavigate } from 'react-router-dom'
+
 
 export default function LoginPage(){
-  
-  const {setUsername} = useContext(Context);
+  const { username, setUsername } = useContext(Context);
+  const navigate = useNavigate();
 
   const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>):void => {
     let value = e.target.value;
     value = value.replace(/[^a-zA-Z\s]/g, '');
     setUsername(value);
   }
+
+  const handleSubmit = () =>{
+    if(username === null || username === '') return;
+    navigate('/game')
+  }
   return (
     <form>
       <h3>Enter your name:</h3>
       <input type='text' placeholder="name:" onChange={handleNameChange}/>
-      <button>Start</button>
+      <button onClick={handleSubmit}>Start</button>
     </form>
   )
 }
