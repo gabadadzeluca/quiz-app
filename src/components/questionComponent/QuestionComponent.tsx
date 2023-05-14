51b8520cd0126edfa4f85e478d4a959a566a2d9a
Luca <gabadadzeluca@gmail.com>
2023-05-14 23:55:54 +0400

    add types to props; Pass additional props to change question number

--- src/components/questionComponent/QuestionComponent.tsx
+++ src/components/questionComponent/QuestionComponent.tsx
@@ -1,15 +1,23 @@
 import { QuestionType } from "../../utils/QuestionType";
+import { useState } from "react";
 
-export default function QuestionComponent(props: {question:QuestionType}){
+export default function QuestionComponent(props: {
+  question:QuestionType;
+  currentQuestionIndex: number;
+  setCurrentQuestionIndex: (index: number)=>void;
+}){
+  const [ correctAnswers, setCorrectAnswers ] = useState<number>(0);
   const { question, correct_answer, incorrect_answers, } = props.question;
+  const { currentQuestionIndex, setCurrentQuestionIndex } = props;
   const answers = [...incorrect_answers, correct_answer];
   const shuffledAnswers = answers.sort(() => Math.random() - 0.5);
+
   return (
     <div>
       <p>{atob(question)}</p>
       <div>
         {shuffledAnswers.map((answer) => 
-          <li>{atob(answer)}</li>
+          <li onClick={() => handleAnswer(answer)}>{atob(answer)}</li>
         )}
       </div>
     </div>
