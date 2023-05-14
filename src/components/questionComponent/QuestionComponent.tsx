f4cd75b57a8c5afd76c8c7171ea865f6b27aefed
Luca <gabadadzeluca@gmail.com>
2023-05-14 23:23:07 +0400

    add shuffledAnswers array to use instead of answers

--- src/components/questionComponent/QuestionComponent.tsx
+++ src/components/questionComponent/QuestionComponent.tsx
@@ -3,12 +3,12 @@ import { QuestionType } from "../../utils/QuestionType";
 export default function QuestionComponent(props: {question:QuestionType}){
   const { question, correct_answer, incorrect_answers, } = props.question;
   const answers = [...incorrect_answers, correct_answer];
-  console.log("ANSWERS: ", answers);
+  const shuffledAnswers = answers.sort(() => Math.random() - 0.5);
   return (
     <div>
       <p>{atob(question)}</p>
       <div>
-        {answers.map((answer) => 
+        {shuffledAnswers.map((answer) => 
           <li>{atob(answer)}</li>
         )}
       </div>
