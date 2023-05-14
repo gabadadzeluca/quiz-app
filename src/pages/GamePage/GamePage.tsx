bfb96fa86bf3d3361b8bc4238de0c6eab7ce8e01
Luca <gabadadzeluca@gmail.com>
2023-05-14 23:03:04 +0400

    format document with prettier

--- src/pages/GamePage/GamePage.tsx
+++ src/pages/GamePage/GamePage.tsx
@@ -1,46 +1,39 @@
-import { useContext, useEffect, useState } from 'react'
-import { Context } from '../../App'
-import axios from 'axios';
-import { QuestionType } from '../../utils/QuestionType';
-import QuestionComponent from '../../components/questionComponent/QuestionComponent';
+import { useContext, useEffect, useState } from "react";
+import { Context } from "../../App";
+import axios from "axios";
+import { QuestionType } from "../../utils/QuestionType";
+import QuestionComponent from "../../components/questionComponent/QuestionComponent";
 
-export default function GamePage(){
-  const { 
-    username,
-    number,
-    categoryId,
-    difficulty
-  } = useContext(Context);
+export default function GamePage() {
+  const { username, number, categoryId, difficulty } = useContext(Context);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
 
   const API_URL = `https://opentdb.com/api.php?amount=${number}&category=${categoryId}&difficulty=${difficulty}&encode=base64`;
-  const[questions, setQuestions] = useState<QuestionType[]|[]>([]);
+  const [questions, setQuestions] = useState<QuestionType[] | []>([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchQuestions();
   }, []);
-  const fetchQuestions =  async () =>{
+  const fetchQuestions = async () => {
     const response = await axios.get(API_URL);
     setQuestions(response.data?.results);
-  } 
+  };
 
   const handleQuestionChange = () => {
-    console.log("Q NUMBER: ",currentQuestionIndex+1);
-    if(currentQuestionIndex + 1 === number) return; // last question => add function to end the game
+    console.log("Q NUMBER: ", currentQuestionIndex + 1);
+    if (currentQuestionIndex + 1 === number) return; // last question => add function to end the game
     setCurrentQuestionIndex(currentQuestionIndex + 1);
-  }
+  };
 
   console.log(questions[currentQuestionIndex]);
   return (
     <div>
       <>
-        {questions.length > 0 ? 
-        <QuestionComponent question={questions[currentQuestionIndex]} />
-          :
-          null
-        }
+        {questions.length > 0 ? (
+          <QuestionComponent question={questions[currentQuestionIndex]} />
+        ) : null}
       </>
       <button onClick={handleQuestionChange}>NEXT</button>
     </div>
-  )
+  );
 }
