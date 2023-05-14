4345dfe685a090fccce09b7aec9752cf2439501c
Luca <gabadadzeluca@gmail.com>
2023-05-14 22:08:14 +0400

    add starting code for fetching questions(base64) format;

--- src/pages/GamePage/GamePage.tsx
+++ src/pages/GamePage/GamePage.tsx
@@ -1,7 +1,39 @@
+import { useContext, useEffect, useState } from 'react'
+import { Context } from '../../App'
+import axios from 'axios';
+
+type QuestionType = {
+  category: string;
+  correct_answer: string;
+  incorrect_answers: string[];
+  question: string;
+}
+
 export default function GamePage(){
+  const { 
+    username,
+    number,
+    categoryId,
+    difficulty
+  } = useContext(Context);
+
+  const API_URL = `https://opentdb.com/api.php?amount=${number}&category=${categoryId}&difficulty=${difficulty}&encode=base64`;
+  const[questions, setQuestions] = useState<QuestionType[]|[]>([]);
+  console.log("questions:", questions);
+
+  useEffect(()=>{
+    fetchQuestions();
+  }, []);
+  const fetchQuestions =  async () =>{
+    const response = await axios.get(API_URL);
+    console.log(response.data);
+    setQuestions(response.data?.results);
+  } 
   return (
     <div>
-      GAMEPAGE
+      {questions.map((question)=>
+        <p>{atob(question.question)}</p>
+      )}
     </div>
   )
 }
