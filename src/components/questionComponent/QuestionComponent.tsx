4a8f26c9c04df0422a3023f496493f6ceeb16416
Luca <gabadadzeluca@gmail.com>
2023-05-15 00:12:05 +0400

    change the name of prop to actual name of the function: handleQuestionChange

--- src/components/questionComponent/QuestionComponent.tsx
+++ src/components/questionComponent/QuestionComponent.tsx
@@ -4,18 +4,19 @@ import { useState } from "react";
 export default function QuestionComponent(props: {
   question:QuestionType;
   currentQuestionIndex: number;
-  setCurrentQuestionIndex: (index: number)=>void;
+  handleQuestionChange: (index: number)=>void;
 }){
   const [ correctAnswers, setCorrectAnswers ] = useState<number>(0);
   const { question, correct_answer, incorrect_answers, } = props.question;
-  const { currentQuestionIndex, setCurrentQuestionIndex } = props;
+  const { currentQuestionIndex, handleQuestionChange } = props;
   const answers = [...incorrect_answers, correct_answer];
   const shuffledAnswers = answers.sort(() => Math.random() - 0.5);
   
   const handleAnswer = (selectedAnswer: string) =>{
     if(selectedAnswer === correct_answer){
+      console.log("CORRECT!");
       setCorrectAnswers(correctAnswers+1);
-      setCurrentQuestionIndex(currentQuestionIndex + 1);
+      handleQuestionChange(currentQuestionIndex + 1);
     }else{ // test
       console.log("INCORRECT!!!")
     }
