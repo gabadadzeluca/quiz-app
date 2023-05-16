1d3db65cd728e041563da324004bd1e25ca40e90
Luca <gabadadzeluca@gmail.com>
2023-05-17 01:10:38 +0400

    add gameFinish prop and its type

--- src/components/questionComponent/QuestionComponent.tsx
+++ src/components/questionComponent/QuestionComponent.tsx
@@ -5,21 +5,21 @@ export default function QuestionComponent(props: {
   question:QuestionType;
   currentQuestionIndex: number;
   handleQuestionChange: (index: number)=>void;
+  finishGame: boolean;
 }){
   const [ correctAnswers, setCorrectAnswers ] = useState<number>(0);
   const { question, correct_answer, incorrect_answers, } = props.question;
-  const { currentQuestionIndex, handleQuestionChange } = props;
+  const { currentQuestionIndex, handleQuestionChange, finishGame} = props;
   const answers = [...incorrect_answers, correct_answer];
   const shuffledAnswers = answers.sort(() => Math.random() - 0.5);
-  
   const handleAnswer = (selectedAnswer: string) =>{
     if(selectedAnswer === correct_answer){
       console.log("CORRECT!");
       setCorrectAnswers(correctAnswers+1);
-      handleQuestionChange(currentQuestionIndex + 1);
-    }else{ // test
+    }else{
       console.log("INCORRECT!!!")
     }
+    handleQuestionChange(currentQuestionIndex + 1);
   }
   return (
     <div>
