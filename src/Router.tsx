d3752b9b8d92636884f937b95a5a132e66546062
Luca <gabadadzeluca@gmail.com>
2023-05-18 23:47:50 +0400

    add LeaderboardPage to its route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import LoginPage from './pages/LoginPage/LoginPage'
 import GamePage from './pages/GamePage/GamePage'
+import LeaderboardPage from './pages/LeaderboardPage/LeaderboardPage'
 
 export default function Router(){
   return (
   <Routes>
     <Route path="/" element={<LoginPage />} />
     <Route path="/game" element={<GamePage />} />
-    <Route path="/leaderboard" element={<></>} />
+    <Route path="/leaderboard" element={<LeaderboardPage />} />
   </Routes>
   )
 }
