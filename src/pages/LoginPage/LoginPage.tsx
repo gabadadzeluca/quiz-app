e1307156bab95c2311fccb0b5a871b111b440223
Luca <gabadadzeluca@gmail.com>
2023-05-19 13:30:19 +0400

    add dropdown menu with leaderboard to login page

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Context } from "../../App";
 import { useNavigate } from "react-router-dom";
 import {
@@ -6,12 +6,13 @@ import {
   difficultyOptions,
   numberOptions,
 } from "../../utils/options";
+import LeaderboardPage from "../LeaderboardPage/LeaderboardPage";
 
 export default function LoginPage() {
   const { username, number, setUsername, setCategoryId, setDifficulty, setNumber } =
     useContext(Context);
   const navigate = useNavigate();
-
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   // create a leaderboard object
   const leaderboardObj = localStorage.getItem("leaderboard");
   if (!leaderboardObj || !Array.isArray(JSON.parse(leaderboardObj))) {
@@ -53,50 +54,59 @@ export default function LoginPage() {
   };
 
   return (
-    <form>
+    <div>
+      <form>
+        <div>
+          <div>
+            <label>Name:</label>
+            <input
+              type="text"
+              placeholder="name:"
+              onChange={handleNameChange}
+              value={username ?? ""}
+            />
+          </div>
+
+          <div>
+            <label>Number of questions:</label>
+            <select onChange={handleNumberChange}>
+              {numberOptions.map((number) => (
+                <option value={number} key={number}>
+                  {number}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div>
+            <label>Category:</label>
+            <select onChange={handleCategoryChange}>
+              {categoryOptions.map((obj) => (
+                <option key={obj.id} value={obj.id}>
+                  {obj.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <label>Difficulty:</label>
+            <select onChange={handleDifficultyChange}>
+              {difficultyOptions.map((option) => (
+                <option value={option}>{option}</option>
+              ))}
+            </select>
+          </div>
+        </div>
+
+        <button onClick={handleSubmit}>Start</button>
+      </form>
+
       <div>
-        <div>
-          <label>Name:</label>
-          <input
-            type="text"
-            placeholder="name:"
-            onChange={handleNameChange}
-            value={username ?? ""}
-          />
-        </div>
-
-        <div>
-          <label>Number of questions:</label>
-          <select onChange={handleNumberChange}>
-            {numberOptions.map((number) => (
-              <option value={number} key={number}>
-                {number}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        <div>
-          <label>Category:</label>
-          <select onChange={handleCategoryChange}>
-            {categoryOptions.map((obj) => (
-              <option key={obj.id} value={obj.id}>
-                {obj.name}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div>
-          <label>Difficulty:</label>
-          <select onChange={handleDifficultyChange}>
-            {difficultyOptions.map((option) => (
-              <option value={option}>{option}</option>
-            ))}
-          </select>
-        </div>
+        <button onClick={()=>setIsOpen(!isOpen)}>LEADERBOARD</button>
+        {isOpen && (
+          <LeaderboardPage />
+        )}
       </div>
-
-      <button onClick={handleSubmit}>Start</button>
-    </form>
+    </div>
   );
 }
