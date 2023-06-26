9fb443dbb22c0964c3f976e8e2f5c953c7fda538
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-06-26 21:51:31 +0400

    add home link to leaderboard

--- src/pages/LeaderboardPage/LeaderboardPage.tsx
+++ src/pages/LeaderboardPage/LeaderboardPage.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { ScoreObjectType } from "../../utils/ScoreObjectType";
 import styles from "./LeaderBoardPage.module.css";
 
@@ -15,6 +16,11 @@ export default function LeaderboardPage() {
           </li>
         ))}
       </ul>
+      <button className={styles.button}>
+        <Link to={"/#/"} className={styles.link}>
+          Home
+        </Link>
+      </button>
     </div>
   );
 }
